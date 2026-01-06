d8a81675c412618458565938f14a41076350c9e4
Mo <byboldbrain@gmail.com>
2026-01-07 00:29:05 +0200

    Initial Navienty web app

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>Navienty</h1>
+      <p>Restaurant Operating System</p>
+    </main>
+  );
+}
